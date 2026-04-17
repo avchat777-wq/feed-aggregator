c91e31f697f5e5f78804872596026448593ad747
Andrey <avchat777@mail.com>
2026-04-17 22:37:34 +0700

    fix: убрал дублирование /api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: '' })
 
 // Add auth token to requests
 api.interceptors.request.use((config) => {
